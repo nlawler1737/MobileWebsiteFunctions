92c53fa59e36537226521db980a55c782ae5bc97
nlawler1737 <72905287+nlawler1737@users.noreply.github.com>
2022-05-07 14:22:30 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,10 +2,12 @@ console.log('func started')
 window.addEventListener("load",function(){console.log("STARTED?");setup()})
 function setup(){
 console.log("setup started")
-  const urlHref = window.location.href
-  const urlHost = window.location.host
-  const urlPathname = window.location.pathname
-  if(urlHref == 'https://www.tvpassport.com/tv-listings'){
+  const url = {
+    href:window.location.href,
+    host:window.location.host,
+    pathname:window.location.pathname
+  }
+  if(url.href == 'https://www.tvpassport.com/tv-listings'){
     console.log("through if")
       let channelNums = [2,3,4,5,6,7,8,9,11,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,66,68,69,70,71,72,73,74,75,77,80,84,90,92,98,102,103,104,105,106,107,108,111,112,113,116,117,118,119,121,122,123,125,126,132,143,159,160,162,165,166,167,168,169,170,171,172,175,176,181,188,194,201,202,203,204,205,206,211,225,226,251,258,264,293,300,301,320,323,325,400,442,461,472,480,483,487,496,511,531,551,571,581,630,827,953,1200,1203,1206,1209,1212,1221,1224,1226,1228,1230,1231,1232,1256,1277,1280,1357,1358,1390]
     document.querySelectorAll(".channel_col").forEach(e => {
@@ -14,7 +16,7 @@ console.log("setup started")
       if (channelNums.includes(Number(arr[arr.length-1]))) {e.children[0].style.backgroundColor = "palegreen"}
     })
   }
-  if (urlHost == "m.youtube.com") {
+  if (url.host == "m.youtube.com") {
     console.log("YouTube")
     if (document.querySelector("#pip-button")) document.querySelector("#pip-button").remove()
     let controls = document.body
